a4f8cd574eb8b746e49ec9c4e53e89045113ad4a
Abhi <abhim.moharana@gmail.com>
2019-04-17 12:22:56 +0530

    Added @PathParam annotation and based on that exception will b risen and handelled

--- mvc_ExceptionHandelling/src/main/java/org/exception/example/controllers/SignUpController.java
+++ mvc_ExceptionHandelling/src/main/java/org/exception/example/controllers/SignUpController.java
@@ -1,8 +1,6 @@
 package org.exception.example.controllers;
 
 
-import java.io.IOException;
-
 import javax.validation.Valid;
 
 import org.exception.example.beans.UserSignup;
@@ -12,32 +10,48 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.util.pattern.PathPattern;
 
 @Controller
 public class SignUpController
 {
-   @GetMapping(value="/signup")
-   public String displayForm(Model mod,@RequestParam("exc")String exp)
+   @GetMapping(value="/signup/{exc}")
+   public String displayForm(Model mod,@PathVariable("exc")String exp)
    {
+	   //String exp="are";
 	   mod.addAttribute("usersign",new UserSignup());
 	   if(exp.equalsIgnoreCase("null"))
+	   {
+		   System.out.println("serching for null pointer exception");
 		   throw new NullPointerException();
+	   }
 	   if(exp.equalsIgnoreCase("are"))
+	   {
+		   System.out.println("serching for arethmatic exception");
 		   throw new ArithmeticException();
+	   }
 	   return "signupform";
    }
    
    
    @ExceptionHandler(value=NullPointerException.class)
-   public String throughException(Model mod)
+   public String throughNullPException(Model mod)
    {
 	   String sms="NullPointer Exception occured, contact to the admin";
 	   mod.addAttribute("nullsms",sms);
 	   return "null";
    }
+   
+   @ExceptionHandler(value=ArithmeticException.class)
+   public String throughArethMException(Model mod)
+   {
+	   String sms="Arethematic Exception occured, check ur inputs";
+	   mod.addAttribute("arth",sms);
+	   return "arth";
+   }
+   
    
    
    @PostMapping(value="/dosignup")
